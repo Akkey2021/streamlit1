6d988dacda08fa3247a3928bc5ff8aa039645afb
Akkey2021 <83562814+Akkey2021@users.noreply.github.com>
2021-05-13 23:26:59 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -53,10 +53,10 @@ if uploaded_image is not None:
         h=object.rectangle.h
         capt=object.object_property
 
-        font=ImageFont.truetype(font='./Helvetica 400.ttf', size=50) #font情報
+        font=ImageFont.truetype(font='./Helvetica 400.ttf', size=100) #font情報
         text_w, text_h = draw.textsize(capt,font=font)
 
-        draw.rectangle([(x,y),(x+w),(y+h)],fill=None, outline='green',width=4)
+        draw.rectangle([(x,y),(x+w),(y+h)],fill=None, outline='green',width=10)
         draw.rectangle([(x,y),(x+text_w),(y+text_h)],fill='green')
         draw.text((x,y),capt,fill='white',font=font)
     st.image(img) #画像の表示
